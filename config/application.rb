f52599100ab87c1b4136cbceba274e8bce83f222
Mmadov <mmadov95@gmail.com>
2016-11-25 16:02:34 +0400

    first commit

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,12 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.i18n.default_locale = :az
 module Eurogas
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+config.i18n.default_locale = :az
   end
 end
